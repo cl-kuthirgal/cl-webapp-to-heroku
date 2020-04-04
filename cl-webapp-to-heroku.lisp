384e7c815a4bfaf1dff452bc52a4ebd67c307223
spainisnotequal <spainisnotequal@gmail.com>
2020-04-04 16:58:10 +0200

    Remove unnecessary code

--- cl-webapp-to-heroku.lisp
+++ cl-webapp-to-heroku.lisp
@@ -4,15 +4,11 @@
 
 (defvar *acceptor* nil "the hunchentoot acceptor")
 
-;; Set the directory to serve
-;; (defparameter *path-to-www* (uiop:merge-pathnames* (uiop:parse-unix-namestring "./www/")))
-
 (defun start-server (&optional (port 8080))
   (stop-server)
   (setf *acceptor* (make-instance 'hunchentoot:acceptor
                                   :port port
-                                  :document-root (uiop:parse-unix-namestring "./www/"))) ;; 
-  ;;(setf (hunchentoot:acceptor-document-root *acceptor*) *path-to-www*)  ;; if you include the option :document-root in the above expression, this line is not necessary anymore
+                                  :document-root (uiop:parse-unix-namestring "./www/")))
   (hunchentoot:start *acceptor*))
 
 (defun stop-server ()
